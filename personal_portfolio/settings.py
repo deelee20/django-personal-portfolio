cbe7fafff99c883eccb5f0a6a4c2c02ac85f291f
Dillon Lee <dillon_lee@live.com>
2020-05-18 22:28:47 -0500

    local settings add on again

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,3 +127,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file, you must be live!")
