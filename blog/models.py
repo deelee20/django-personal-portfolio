97e60d614726b3b602f670bc9053424c02f33b40
Dillon Lee <dillon_lee@live.com>
2020-06-25 12:25:06 -0500

    local settings update june

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,4 @@ class Blog(models.Model):
 
  	def __str__(self):
  		return self.title
-
+ 		
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from .models import Project
 
 admin.site.register(Project)
+admin.site.site_header = 'DeeLee'
